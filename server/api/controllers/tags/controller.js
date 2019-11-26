f2457894aee62991cfc445323439714c892a0ca8
Prathma Rastogi <prathma999@gmail.com>
2019-11-25 23:24:05 -0500

    Tags api

---
+++ server/api/controllers/tags/controller.js
@@ -0,0 +1,20 @@
+import Tags from '../../models/tags';
+import l from '../../../common/logger';
+
+export class Controller {
+  all(req, res) {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 10;
+    let offset = req.query.offset ? parseInt(req.query.offset) : 0;
+    Tags.find()
+    .limit(limit)
+    .skip(offset)
+    .then(tags => {
+      return res.json(tags);
+    })
+    .catch(err => {
+      l.error(err);
+      res.status(422).json(err.errors);
+    });
+  }
+}
+export default new Controller();
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,8 @@
 import postsRouter from './api/controllers/posts/router';
+import tagsRouter from './api/controllers/tags/router';
 
 export default function routes(app) {
+	console.log("Debugg");
   app.use('/api/v1/posts', postsRouter);
+  app.use('/api/v1/tags', tagsRouter);
 }
