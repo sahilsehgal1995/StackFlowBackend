617bec3816c950b1295f79eea46a6c6f306dc5c2
Prathma Rastogi <prathma999@gmail.com>
2019-11-27 17:42:22 -0500

    change added for empty string search

--- server/api/controllers/tags/controller.js
+++ server/api/controllers/tags/controller.js
@@ -5,16 +5,29 @@ export class Controller {
   all(req, res) {
     let limit = req.query.limit ? parseInt(req.query.limit) : 10;
     let offset = req.query.offset ? parseInt(req.query.offset) : 0;
-    Tags.find({tag_name: {$regex: '^' + req.params.tag_name, $options: 'i'}})
-    .limit(limit)
-    .skip(offset)
-    .then(tags => {
-      return res.json(tags);
-    })
-    .catch(err => {
-      l.error(err);
-      res.status(422).json(err.errors);
-    });
+    if (!req.params.tag_name) {
+      Tags.find()
+      .limit(limit)
+      .skip(offset)
+      .then(tags => {
+        return res.json(tags);
+      })
+      .catch(err => {
+        l.error(err);
+        res.status(422).json(err.errors);
+      });
+    } else {
+      Tags.find({tag_name: {$regex: '^' + req.params.tag_name, $options: 'i'}})
+      .limit(limit)
+      .skip(offset)
+      .then(tags => {
+        return res.json(tags);
+      })
+      .catch(err => {
+        l.error(err);
+        res.status(422).json(err.errors);
+      });
+    }
   }
 }
 export default new Controller();
--- server/api/controllers/tags/router.js
+++ server/api/controllers/tags/router.js
@@ -4,3 +4,4 @@ import controller from './controller';
 export default express
   .Router()
   .get('/:tag_name', controller.all)
+  .get('/', controller.all)
